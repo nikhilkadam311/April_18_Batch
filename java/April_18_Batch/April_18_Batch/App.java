52e434c40959c089d8ab867a85f707a2e4a0f02a
SonalKadam8888 <sonalkadam1234@gmail.com>
2022-10-27 10:45:22 +0530

    First commit

---
+++ java/April_18_Batch/April_18_Batch/App.java
@@ -0,0 +1,13 @@
+package April_18_Batch.April_18_Batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
